e597d6489ea1714fc028d3dc90da89542fdc67aa
Chance Mims <chance@Chances-MacBook-Air.local>
2020-01-13 18:19:14 -0600

    Done.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,39 @@
+class ArtistsController < ApplicationController
+    before_action :find_artist, only: [:show, :edit, :update]
+
+    def show
+    end
+
+    def index
+        @artists = Artist.all
+    end
+
+    def new
+        @artist = Artist.new
+    end
+
+    def create
+        @artist = Artist.create(artist_params(:name, :bio))
+        redirect_to artist_path(@artist)
+    end
+
+    def edit
+    end
+
+    def update
+        @artist = Artist.update(artist_params(:name, :bio))
+        redirect_to artist_path(@artist)
+    end
+
+    private
+
+    def find_artist
+        @artist = Artist.find(params[:id])
+    end
+
+    def artist_params(*args)
+        params.require(:artist).permit(*args)
+    end
+
+
+end
